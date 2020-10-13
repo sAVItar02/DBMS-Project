1c8e17ef7177641df56cd75f53349723461334db
Aviral Singh Chauhan <59026666+sAVItar02@users.noreply.github.com>
2020-10-14 00:58:15 +0530

    Bug fixes

--- Public/js/flights.js
+++ Public/js/flights.js
@@ -1,9 +1,19 @@
 $('.card-popup').hide();
+$('.overlay').hide();
 $(document).ready(function() {
 
     $('.details').on('click', function(e) {
         e.preventDefault();
         $(this).parent().parent().parent().children('.card-popup').show();
+        $('.overlay').show();
+        $(this).parent().parent().parent().removeClass('card-hover');
+
+        $('.close').on('click', (e) => {
+            e.preventDefault();
+            $('.card-popup').hide();
+            $('.overlay').hide();
+            $(this).parent().parent().parent().addClass('card-hover');
+        })
     })
 
 });
