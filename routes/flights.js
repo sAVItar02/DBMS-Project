cc3b8d57fd11fbcd8b7888ca269e32b47e00347d
Aviral Singh Chauhan <59026666+sAVItar02@users.noreply.github.com>
2020-10-15 03:02:56 +0530

    check deployment

--- routes/flights.js
+++ routes/flights.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = new express.Router();
 
 router.get('/flights', function(req, res){
-    console.log(req.query);
+    res.send(req.query);
 });
 
 module.exports = router;
